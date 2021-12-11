5b700974e92b3a110d7e8eeeb25436663d64d0ca
naveenreddivari2019 <naveen.reddivari@gmail.com>
2021-12-11 21:35:19 +0100

    Commit

--- src/main/java/com/sb/SpringBootFormHandingApplication.java
+++ src/main/java/com/sb/SpringBootFormHandingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootFormHandingApplication {
-
+//main
     public static void main(String[] args) {
         SpringApplication.run(SpringBootFormHandingApplication.class, args);
     }
